9983a1243df5eb77394d7accfdae7962077b561d
Vishal Mangla <mangla@gmail.com>
2022-02-07 17:13:37 -0800

    Initial robots fighting each other

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,6 +14,19 @@ console.log (enemyName, enemyHealth, enemyAttack)
 
 var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth =enemyHealth - playerAttack
+    
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+      );
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+  // Log a resulting message to the console so we know that it worked.
+  console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+  );
     
 };
 
