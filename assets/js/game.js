5583cd741b3ab0171ef3ae8fde6c88c1173205c5
Vishal Mangla <mangla@gmail.com>
2022-02-07 16:58:48 -0800

    Initial Game variable definations

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,21 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack =10;
+
+//You can also log multiple values at once like
+
+console.log (playerName, playerHealth, playerAttack);
+
+var enemyName ="Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+console.log (enemyName, enemyHealth, enemyAttack)
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+    
+};
+
+fight();
+
